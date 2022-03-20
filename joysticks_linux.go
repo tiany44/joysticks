ec93e542ccd7ffcf09071b7eb760456e6342196c
Nigel Tiany <98428421+tiany44@users.noreply.github.com>
2022-03-20 15:43:42 -0400

    Fix incorrect event indexing
    Fix incorrect event indexing

--- joysticks_linux.go
+++ joysticks_linux.go
@@ -1,6 +1,7 @@
+//go:build linux
 // +build linux
 
-package joysticks
+package joystick
 
 import (
 	"encoding/binary"
@@ -26,20 +27,20 @@ var inputPathSlice = []byte("/dev/input/js ")[0:13]
 
 // see if Device exists.
 func DeviceExists(index uint8) bool {
-	_,err:=os.Stat(string(strconv.AppendUint(inputPathSlice, uint64(index-1), 10)))
-	return err==nil
+	_, err := os.Stat(string(strconv.AppendUint(inputPathSlice, uint64(index-1), 10)))
+	return err == nil
 }
 
 // Connect sets up a go routine that puts a joysticks events onto registered channels.
 // to register channels use the returned HID object's On<xxx>(index) methods.
 // Note: only one event, of each type '<xxx>', for each 'index', so re-registering, or deleting, an event stops events going on the old channel.
-// It Needs the HID objects ParcelOutEvents() method to be running to perform routing.(so usually in a go routine.) 
+// It Needs the HID objects ParcelOutEvents() method to be running to perform routing.(so usually in a go routine.)
 func Connect(index int) (d *HID) {
 	r, e := os.OpenFile(string(strconv.AppendUint(inputPathSlice, uint64(index-1), 10)), os.O_RDWR, 0)
 	if e != nil {
 		return nil
 	}
-	d = &HID{make(chan osEventRecord), make(map[uint8]button), make(map[uint8]hatAxis), make(map[eventSignature]chan Event), &sync.RWMutex{},&sync.RWMutex{}, &sync.RWMutex{}}
+	d = &HID{make(chan osEventRecord), make(map[uint8]button), make(map[uint8]hatAxis), make(map[eventSignature]chan Event), &sync.RWMutex{}, &sync.RWMutex{}, &sync.RWMutex{}}
 	// start thread to read joystick events to the joystick.state osEvent channel
 	go eventPipe(r, d.OSEvents)
 	d.populate()
@@ -48,19 +49,13 @@ func Connect(index int) (d *HID) {
 
 // fill in the joysticks available events from the synthetic events burst produced initially by the driver.
 func (d HID) populate() {
-	for buttonNumber, hatNumber, axisNumber := 1, 1, 1; ; {
+	for {
 		evt := <-d.OSEvents
 		switch evt.Type {
 		case 0x81:
-			d.Buttons[evt.Index] = button{uint8(buttonNumber), toDuration(evt.Time), evt.Value != 0}
-			buttonNumber += 1
+			d.Buttons[evt.Index] = button{evt.Index, toDuration(evt.Time), evt.Value != 0}
 		case 0x82:
-			d.HatAxes[evt.Index] = hatAxis{uint8(hatNumber), uint8(axisNumber), false, toDuration(evt.Time), float32(evt.Value) / maxValue}
-			axisNumber += 1
-			if axisNumber > 2 {
-				axisNumber = 1
-				hatNumber += 1
-			}
+			d.HatAxes[evt.Index] = hatAxis{evt.Index, evt.Index, false, toDuration(evt.Time), float32(evt.Value) / maxValue}
 		default:
 			go func() { d.OSEvents <- evt }() // have to consume a real event to know we reached the end of the synthetic burst, so refire it.
 			return
